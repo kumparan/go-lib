58cbf402b1a81eb161428398f4fb2812e848bc49
Aslam Hadi H <aslamhadi@gmail.com>
2018-04-25 17:46:04 +0700

    add util to generate slug from string (#5)

--- utils/string.go
+++ utils/string.go
@@ -6,3 +6,8 @@ import "strings"
 func StandardizeSpaces(s string) string {
 	return strings.Join(strings.Fields(s), " ")
 }
+
+// GenerateSlug  -> Replace space with dash, lower it, and trim the space
+func GenerateSlug(inputStr string) string {
+	return strings.Trim(strings.ToLower(strings.Replace(inputStr, " ", "-", -1)), " ")
+}
