f8869956b5dfcc782263b3793577f00ef78fe0a9
Aslam Hadi H <aslamhadi@gmail.com>
2018-04-26 12:54:01 +0700

    add email validation with regex (#6)

---
+++ utils/bool.go
@@ -0,0 +1,9 @@
+package utils
+
+import "regexp"
+
+// IsEmailValid -> validate email using regex
+func IsEmailValid(email string) bool {
+	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	return re.MatchString(email)
+}
