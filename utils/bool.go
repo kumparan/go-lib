496ec768d82444f803f4efbaaaac7876c7750f2f
Aslam Hadi H <aslamhadi@gmail.com>
2018-04-27 10:47:04 +0700

    add IsNumeric to check input string is int or not (#7)

--- utils/bool.go
+++ utils/bool.go
@@ -1,9 +1,23 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
+	"strconv"
+)
 
 // IsEmailValid -> validate email using regex
 func IsEmailValid(email string) bool {
 	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	return re.MatchString(email)
+}
+
+// IsNumeric -> Check if input string is int
+func IsNumeric(s string) bool {
+	_, err := strconv.Atoi(s)
+
+	if err != nil {
+		return false
+	}
+
+	return true
 }
