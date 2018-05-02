f579a6adab2557b1dda34d333596026e21980254
Aslam Hadi H <aslamhadi@gmail.com>
2018-05-02 12:07:13 +0700

    Redis module (#10)
    * update redis module
    
    * refactor code

--- redis/redis.go
+++ redis/redis.go
@@ -1,43 +1,58 @@
 package redis
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	redigo "github.com/garyburd/redigo/redis"
 )
 
-// NetworkTCP for tcp
-const NetworkTCP = "tcp"
-
-type (
-	// Config of redis
-	Config struct {
-		Address   string `yaml:"address"`
-		MaxIdle   int    `yaml:"maxidle"`
-		MaxActive int    `yaml:"maxactive"`
-		Timeout   int    `yaml:"timeout"`
-	}
-	// Redis struct
-	Redis struct {
-		Pool *redigo.Pool
-	}
+var (
+	// Pool -> redis connection pool
+	Pool *redigo.Pool
 )
 
-// Init redis connection
-func Init(cfg Config) *Redis {
-	pool := &redigo.Pool{
-		MaxIdle:     cfg.MaxIdle,
-		MaxActive:   cfg.MaxActive,
-		IdleTimeout: time.Duration(cfg.Timeout) * time.Second,
-		Wait:        true,
-		Dial: func() (redigo.Conn, error) {
-			return redigo.Dial(NetworkTCP, cfg.Address)
-		},
-	}
-	r := &Redis{Pool: pool}
-	return r
+// Init initialize Redis Pool
+func Init(redisHost string) {
+
+	Pool = newPool(redisHost)
+	cleanupHook()
 }
 
-func (r *Redis) Err() error {
-	return r.Pool.Get().Err()
+func newPool(server string) *redigo.Pool {
+
+	return &redigo.Pool{
+
+		MaxIdle:     100,
+		MaxActive:   10000,
+		IdleTimeout: 240 * time.Second,
+
+		Dial: func() (redigo.Conn, error) {
+			c, err := redigo.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+
+		TestOnBorrow: func(c redigo.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func cleanupHook() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGKILL)
+	go func() {
+		<-c
+		Pool.Close()
+		os.Exit(0)
+	}()
 }
---
+++ redis/utils.go
@@ -0,0 +1,210 @@
+package redis
+
+import (
+	"fmt"
+	"log"
+
+	redigo "github.com/garyburd/redigo/redis"
+)
+
+// Ping nodoc
+func Ping() error {
+
+	conn := Pool.Get()
+	defer conn.Close()
+
+	_, err := redigo.String(conn.Do("PING"))
+	if err != nil {
+		return fmt.Errorf("cannot 'PING' db: %v", err)
+	}
+	return nil
+}
+
+// Pong nodoc
+func Pong() {
+	client := Pool.Get()
+	defer client.Close()
+	n, err := client.Do("INFO")
+	if err != nil {
+		log.Println("ERROR:" + err.Error())
+	}
+	log.Printf("info=%s", n)
+}
+
+// Execute nodoc
+func Execute(command string, key string, value string) interface{} {
+
+	var content interface{}
+	var err error
+
+	client := Pool.Get()
+	defer client.Close()
+
+	if value != "" {
+		content, err = redigo.Values(client.Do(command, key, value))
+	} else {
+		content, err = redigo.Values(client.Do(command, key))
+	}
+
+	if err != nil {
+		log.Println("ERROR:" + err.Error())
+	}
+
+	return content
+}
+
+// Expire nodoc
+func Expire(key string, redisTTL int64) {
+	client := Pool.Get()
+	defer client.Close()
+
+	_, err := client.Do("EXPIRE", key, redisTTL)
+
+	if err != nil {
+		log.Println("ERROR EXPIRE:" + key + ":" + err.Error())
+	}
+}
+
+// Set nodoc
+func Set(key string, value string, redisTTL int64) {
+	client := Pool.Get()
+	defer client.Close()
+
+	_, err := client.Do("SET", key, value)
+
+	if err != nil {
+		log.Println("ERROR SET:" + key + ":" + err.Error())
+	}
+
+	Expire(key, redisTTL)
+}
+
+// Get nodoc
+func Get(key string) string {
+	client := Pool.Get()
+	defer client.Close()
+
+	isExists, err := redigo.Int(client.Do("EXISTS", key))
+	if err != nil {
+		log.Println("ERROR EXISTS: " + key + ":" + err.Error())
+	}
+
+	if isExists == 0 {
+		// data not exists/expired from redis
+		return ""
+	}
+
+	content, err := redigo.String(client.Do("GET", key))
+
+	if err != nil {
+		log.Println("ERROR GET:" + key + ":" + err.Error())
+	}
+
+	return content
+}
+
+// Delete nodoc
+func Delete(key string) {
+
+	client := Pool.Get()
+	defer client.Close()
+
+	_, err := client.Do("DEL", key)
+
+	if err != nil {
+		log.Println("ERROR DEL:" + key + ":" + err.Error())
+	}
+
+}
+
+// HSet nodoc
+func HSet(key string, key2 string, value string, redisTTL int64) {
+	client := Pool.Get()
+	defer client.Close()
+
+	_, err := client.Do("HSET",
+		key, key2, value)
+
+	if err != nil {
+		log.Println("ERROR HSET:" + key + ":" + key2 + ":" + err.Error())
+	}
+
+	Expire(key, redisTTL)
+}
+
+// HGet nodoc
+func HGet(key string, key2 string) string {
+	client := Pool.Get()
+	defer client.Close()
+
+	n, err := client.Do("HGET", key, key2)
+	if err != nil {
+		log.Println("ERROR HGET1:" + key + ":" + key2 + ":" + err.Error())
+	}
+
+	if n != nil {
+		content, err := redigo.String(client.Do("HGET", key, key2))
+
+		if err != nil {
+			log.Println("ERROR HGET2:" + key + ":" + key2 + ":" + err.Error())
+		}
+
+		return content
+	} else {
+		return "nil"
+	}
+}
+
+// SAdd nodoc
+func SAdd(key string, value string, redisTTL int64) {
+	client := Pool.Get()
+	defer client.Close()
+
+	_, err := client.Do("SADD", key, value)
+
+	if err != nil {
+		log.Println("ERROR SADD:" + key + ":" + err.Error())
+	}
+
+	Expire(key, redisTTL)
+}
+
+// SMembers nodoc
+func SMembers(key string) interface{} {
+	client := Pool.Get()
+	defer client.Close()
+
+	content, err := redigo.Values(client.Do("SMEMBERS", key))
+
+	if err != nil {
+		log.Println("ERROR SMEMBERS:" + key + ":" + err.Error())
+	}
+
+	return content
+}
+
+// SISMember nodoc
+func SISMember(key string, value string) int {
+	client := Pool.Get()
+	defer client.Close()
+
+	content, err := redigo.Int(client.Do("SISMEMBER", key, value))
+
+	if err != nil {
+		log.Println("ERROR SISMEMBER:" + key + ":" + err.Error())
+	}
+
+	return content
+}
+
+// SRem nodoc
+func SRem(key string, value string) {
+	client := Pool.Get()
+	defer client.Close()
+
+	_, err := client.Do("SREM", key, value)
+
+	if err != nil {
+		log.Println("ERROR SREM:" + key + ":" + err.Error())
+	}
+}
