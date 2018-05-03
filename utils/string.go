64d3beeadf1124d5fc50a8a3f445785939690532
Agung Hariadi Tedja <agunghariaditedja@yahoo.com>
2018-05-03 18:30:19 +0700

    Add some helper to make it easier parsing data from context

--- utils/string.go
+++ utils/string.go
@@ -1,6 +1,9 @@
 package utils
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // StandardizeSpaces -> Join long query to one line query
 func StandardizeSpaces(s string) string {
@@ -10,4 +13,33 @@ func StandardizeSpaces(s string) string {
 // GenerateSlug  -> Replace space with dash, lower it, and trim the space
 func GenerateSlug(inputStr string) string {
 	return strings.Trim(strings.ToLower(strings.Replace(inputStr, " ", "-", -1)), " ")
+}
+
+// String2Bool :nodoc:
+func String2Bool(s string) bool {
+	if s != "" {
+		i, err := strconv.Atoi(s)
+		if err == nil {
+			return i == 0
+		}
+	}
+	return false
+}
+
+// String2Int64 :nodoc:
+func String2Int64(s string) int64 {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return i
+}
+
+// String2Int64 :nodoc:
+func String2Int64WithDefault(s string, d int64) int64 {
+	i := String2Int64(s)
+	if i == 0 {
+		return d
+	}
+	return i
 }
