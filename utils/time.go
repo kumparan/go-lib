cdf5421824b4d0ba63fdb5eadbdfeddb20654335
Agung Hariadi Tedja <agunghariaditedja@yahoo.com>
2019-01-10 15:44:34 +0700

    Use RFC3339Nano (#65)

--- utils/time.go
+++ utils/time.go
@@ -2,10 +2,10 @@ package utils
 
 import "time"
 
-// FormatTimeRFC3339 Format time according to RFC3339
+// FormatTimeRFC3339 Format time according to RFC3339Nano
 func FormatTimeRFC3339(t *time.Time) (s string) {
 	if t != nil {
-		s = t.Format(time.RFC3339)
+		s = t.Format(time.RFC3339Nano)
 	}
 	return
 }
--- utils/time_test.go
+++ utils/time_test.go
@@ -1,9 +1,10 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTimeFormat(t *testing.T) {
@@ -13,7 +14,7 @@ func TestTimeFormat(t *testing.T) {
 	})
 
 	t.Run("Now", func(t *testing.T) {
-		now, err := time.Parse(time.RFC3339, "2016-06-06T03:55:00Z")
+		now, err := time.Parse(time.RFC3339Nano, "2016-06-06T03:55:00Z")
 		assert.NoError(t, err)
 		s := FormatTimeRFC3339(&now)
 		assert.EqualValues(t, "2016-06-06T03:55:00Z", s)
