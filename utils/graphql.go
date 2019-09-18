0b77873b3a00f16731ffe90ce67e31fbc9b896d2
Dzaka Ammar <dzaka.ibrahim@kumparan.com>
2019-09-18 11:15:44 +0700

    Refactor some code

--- utils/graphql.go
+++ utils/graphql.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"strconv"
 
 	graphql "github.com/graph-gophers/graphql-go"
@@ -9,12 +8,12 @@ import (
 
 // Int642GraphQLID :nodoc:
 func Int642GraphQLID(i int64) graphql.ID {
-	return graphql.ID(fmt.Sprintf("%d", i))
+	return graphql.ID(Int642String(i))
 }
 
 // GraphQLID2String :nodoc:
 func GraphQLID2String(id graphql.ID) string {
-	return fmt.Sprintf("%s", id)
+	return string(id)
 }
 
 // GraphQLIDPointer2String :nodoc:
@@ -23,16 +22,12 @@ func GraphQLIDPointer2String(id *graphql.ID) string {
 		return ""
 	}
 
-	return fmt.Sprintf("%s", *id)
+	return string(*id)
 }
 
 // GraphQLID2Int64 :nodoc:
 func GraphQLID2Int64(id graphql.ID) int64 {
-	newID, err := strconv.Atoi(fmt.Sprintf("%s", id))
-	if err != nil {
-		return int64(0)
-	}
-	return int64(newID)
+	return String2Int64(string(id))
 }
 
 // GraphQLIDPointer2Int64 :nodoc:
@@ -41,16 +36,12 @@ func GraphQLIDPointer2Int64(id *graphql.ID) int64 {
 		return int64(0)
 	}
 
-	newID, err := strconv.Atoi(fmt.Sprintf("%s", *id))
-	if err != nil {
-		return int64(0)
-	}
-	return int64(newID)
+	return String2Int64(string(*id))
 }
 
 // GraphQLID2Int32 :nodoc:
 func GraphQLID2Int32(id graphql.ID) int32 {
-	newID, err := strconv.Atoi(fmt.Sprintf("%s", id))
+	newID, err := strconv.Atoi(string(id))
 	if err != nil {
 		return int32(0)
 	}
@@ -63,7 +54,7 @@ func GraphQLIDPointer2Int32(id *graphql.ID) int32 {
 		return int32(0)
 	}
 
-	newID, err := strconv.Atoi(fmt.Sprintf("%s", *id))
+	newID, err := strconv.Atoi(string(*id))
 	if err != nil {
 		return int32(0)
 	}
